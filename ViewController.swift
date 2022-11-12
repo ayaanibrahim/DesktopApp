4fd0d0bb45beacbee1a393f2fbd0b7ac05839603
Ayaanibrahim <118040724+ayaanibrahim@users.noreply.github.com>
2022-11-12 10:00:50 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  switch
+//
+//  Created by Ayaan ibrahim on 11/12/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
